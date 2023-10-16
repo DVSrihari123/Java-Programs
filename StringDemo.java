0793e91820e3c45a36c5098f9b17305b90e2d821
DVSrihari123 <142669628+DVSrihari123@users.noreply.github.com>
2023-10-16 12:47:08 +0530

    Add files via upload

---
+++ StringDemo.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+public class StringDemo
+{
+	public static void main(String args[]){
+		String str1="Java Program";
+		String str2=new String("Java Program");
+		char c[] = {'S','R','I','H','A','R','I'};
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(c);
+	}
+} 
